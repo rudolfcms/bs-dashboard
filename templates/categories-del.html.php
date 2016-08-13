d8127f89c8375f5b92f6ee949e8ec26999fe82fe
Mikołaj Pich <m.pich@outlook.com>
2016-08-14 01:21:07 +0200

    Add categories del template

---
+++ templates/categories-del.html.php
@@ -0,0 +1,16 @@
+<?php include '_head.html.php';?>
+
+<form method="post" action="<?=$this->path;?>">
+  <div class="panel panel-default">
+    <div class="panel-heading">Czy na pewno chcesz usunąć?</div>
+    <div class="panel-body">
+      <p>Tytuł: <b><?=$this->category->title();?></b></p>
+    </div>
+    <div class="panel-footer">
+      <a href="../" class="btn">Anuluj</a>
+      <input name="delete" type="submit" class="btn btn-danger btn-flat pull-right" value="Usuń">
+    </div>
+  </div>
+</form>
+
+<?php include '_foot.html.php';?>
