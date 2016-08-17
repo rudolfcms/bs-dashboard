e2139345040beb327a88a07440e737530ffb8142
Mikołaj Pich <m.pich@outlook.com>
2016-08-17 19:26:35 +0200

    Add modules list template

---
+++ templates/modules-list.html.php
@@ -0,0 +1,45 @@
+<?php include '_head.html.php'; ?>
+
+<?php if ($this->loop->isItems()): ?>
+<table class="table table-hover table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Nazwa</th>
+      <th>Status</th>
+      <th>Wyłącz/włącz</th>
+      <th>Edytuj</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php while ($this->loop->haveItems()): $a = $this->loop->item(); ?> 
+    <tr>
+      <th><?=$a->id();?></th>
+      <td><?=$a->name();?></td>
+      
+      <?php if ($a->status()): ?>
+      <td class="text-success" style="font-weight:bold">Włączony</td>
+      <td><a href="<?=$a->offUrl();?>" class="btn btn-warning btn-xs">Wyłącz</a></td>
+      
+      <?php else: ?> 
+      <td class="text-muted">Wyłączony</td>
+      <td><a href="<?=$a->onUrl();?>" class="btn btn-success btn-xs">Włącz</a></td>
+      <?php endif;?> 
+      
+      <td><a href="<?=$a->editUrl();?>" class="btn btn-primary btn-xs">Edytuj</a></td>
+    </tr>
+<?php endwhile;?> 
+  </tbody>
+</table>
+
+<?php if ($this->loop->isPagination()): ?> 
+  <nav role="navigation" class="pagination-container">
+    <?=$this->loop->nav(['ul' => 'pagination', 'current' => 'active'], 2);?>
+  </nav>
+<?php endif;?> 
+
+<?php else: ?> 
+  <div class="alert info">Brak modułów do wyświetlenia!</div>
+<?php endif;?> 
+
+<?php include '_foot.html.php'; ?>
