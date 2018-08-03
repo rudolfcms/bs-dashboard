da9b0677faec77971e5358359818651dfe9ad83d
Mikołaj Pich <m.pich@outlook.com>
2018-08-03 20:45:54 +0200

    Migrate to npm

--- templates/login.html.php
+++ templates/login.html.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1"> 
 		<title>Panel logowania | Zespół Szkół w Rokietincy</title>
-		<link href="<?=$this->themePath;?>/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="<?=$this->themePath;?>/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 		<link href="<?=$this->themePath;?>/css/singin.css" rel="stylesheet">
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
--- theme.php
+++ theme.php
@@ -8,13 +8,13 @@ class bs_dashboard extends Theme
     {
         $this->addHeadBefore('<meta http-equiv="X-UA-Compatible" content="IE=edge">');
         $this->addHeadBefore('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
-        $this->addStylesheet($this->path.'/bower_components/bootstrap/dist/css/bootstrap.min.css');
+        $this->addStylesheet($this->path.'/node_modules/bootstrap/dist/css/bootstrap.min.css');
         $this->addStylesheet('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css');
         $this->addStylesheet($this->path.'/css/dashboard.css');
         $this->addStylesheet($this->path.'/css/custom.css');
         $this->addFootBefore('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script>window.jQuery || document.write("<script src=\''.$this->path.'/bower_components/jquery/dist/jquery.min.js\'><script>")</script>');
-        $this->addScript($this->path.'/bower_components/bootstrap/dist/js/bootstrap.min.js');
+        $this->addScript($this->path.'/node_modules/bootstrap/dist/js/bootstrap.min.js');
         $this->addFootAfter('<script>
         $("#nav-sidebar .treeview").on("click", ".caret-container", function(event) {
           event.stopPropagation();
