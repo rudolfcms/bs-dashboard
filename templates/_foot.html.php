0de5295ced0d748aefd8e138366495f44c7dd137
Mikołaj Pich <m.pich@outlook.com>
2018-08-06 23:59:30 +0200

    Fix footer

--- templates/_foot.html.php
+++ templates/_foot.html.php
@@ -7,8 +7,8 @@
 </div>
 </div>
 <footer class="footer">
-    <div class="container">
-        <strong>&copy; <?= date('Y'); ?> <?= NAME; ?></strong>>
+    <div class="container offset-md-2 col-10">
+        <strong>&copy; <?= date('Y'); ?> <?= NAME; ?></strong>
         <div class="pull-right">
             <a href="https://github.com/rudolfcms/rudolf/releases/tag/<?= VER_NAME; ?>">
                 <?= VER_NAME; ?></a></div>
