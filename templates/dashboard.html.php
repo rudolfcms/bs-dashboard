f2f8a809c7c1cd308a6e59e117dd9f966fdeec63
Mikołaj Pich <m.pich@outlook.com>
2018-08-07 00:37:31 +0200

    Add some items on dashboard

--- templates/dashboard.html.php
+++ templates/dashboard.html.php
@@ -1,9 +1,28 @@
 <?php
 
 /** @var \Rudolf\Modules\Dashboard\View $this */
+/** @var \Rudolf\Component\Helpers\Navigation\MenuItem $value */
+
 
 include '_head.html.php'; ?>
 
+<div class="row">
+    <?php foreach ($this->getMenuCollection() as $key => $value): ?>
 
+        <div class="col-4">
+            <div class="card mb-4">
+                <a href="<?=DIR.$value->getSlug();?>" style="text-decoration: none;">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <span class="fa <?=$value->getIco();?> mr-3"></span>
+                            <?=$value->getTitle();?>
+                        </h5>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+    <?php endforeach; ?>
+</div>
 
 <?php include '_foot.html.php';
