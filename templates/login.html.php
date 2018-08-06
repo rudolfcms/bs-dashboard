5ccd126b4050746234a7508493051cc02a6edb9c
Mikołaj Pich <m.pich@outlook.com>
2018-08-06 22:07:46 +0200

    Update login screen too

--- templates/login.html.php
+++ templates/login.html.php
@@ -4,46 +4,33 @@
 
 ?><!DOCTYPE html>
 <html lang="pl">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1"> 
-		<title>Panel logowania | Zespół Szkół w Rokietincy</title>
-		<link href="<?=$this->themePath;?>/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-		<link href="<?=$this->themePath;?>/css/singin.css" rel="stylesheet">
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
-	</head>
-	<body>
-	<div class="container">
-		<form method="post" action="" class="form-horizontal" role="form">
-			<h2 class="form-signin-heading">Zaloguj się</h2>
-			<?php if ($this->isMessage()):?> 
-				<div class="alert alert-<?=$this->getMessage('type');?>"><?=$this->getMessage('message');?></div>
-			<?php endif;?> 
-			<div class="form-group">
-				<label for="email" class="col-sm-2 control-label">Nazwa użytkownika</label>
-				<div class="col-sm-5">
-					<input type="text" value="<?=$this->getNick();?>" class="form-control" id="email" name="email" placeholder="Nazwa użytkownika" autofocus required>
-				</div>
-			</div>
-			<div class="form-group">
-				<label for="password" class="col-sm-2 control-label">Hasło</label>
-				<div class="col-sm-5">
-					<input type="password" class="form-control" id="password" name="password" placeholder="Hasło" required>
-				</div>
-			</div>
-			<div class="form-group">
-				<div class="col-sm-offset-2 col-sm-1">
-					<button type="submit" name="send" class="btn btn-primary">Zaloguj</button>
-				</div>
-				<br><br><br>
-			</div>
-				
-			<a href="<?=DIR;?>/">&#171; Wróć do strony głównej</a>
-		</form>
-	</div>
-	</body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Panel logowania | <?=strip_tags(GENERAL_SITE_NAME);?></title>
+    <link href="<?= $this->themePath; ?>/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= $this->themePath; ?>/css/singin.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+<form class="form-signin" method="post">
+    <img class="mb-4" src="https://avatars3.githubusercontent.com/u/25126979" alt="" width="72" height="72">
+    <h1 class="h3 mb-3 font-weight-normal">Panel zarządzania</h1>
+
+    <?php if ($this->isMessage()): ?>
+        <div class="alert alert-<?= $this->getMessage('type'); ?>"><?= ucfirst($this->getMessage('message')); ?>.</div>
+    <?php endif; ?>
+
+    <label for="inputEmail" class="sr-only">Email</label>
+    <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Email"
+           value="<?= $this->getNick(); ?>" required autofocus>
+
+    <label for="inputPassword" class="sr-only">Hasło</label>
+    <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Hasło" required>
+
+    <button name="send" class="btn btn-lg btn-primary btn-block" type="submit">Zaloguj</button>
+    <p class="mt-5 mb-3 text-muted">&copy; by Rudolf <?= VER_NAME; ?></p>
+</form>
+</body>
+
 </html>
