475d92be19041164b76047d7a0ce5b614a03ad53
Mikołaj Pich <m.pich@outlook.com>
2018-08-07 22:48:29 +0200

    Fix login screen

--- templates/login.html.php
+++ templates/login.html.php
@@ -12,25 +12,27 @@
     <link href="<?= $this->themePath; ?>/css/singin.css" rel="stylesheet">
 </head>
 
-<body class="text-center">
-<form class="form-signin" method="post">
-    <img class="mb-4" src="https://avatars3.githubusercontent.com/u/25126979" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Panel zarządzania</h1>
+<body>
+<div class="wrapper text-center">
+    <form class="form-signin" method="post">
+        <img class="mb-4" src="https://avatars3.githubusercontent.com/u/25126979" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Panel zarządzania</h1>
 
-    <?php if ($this->isMessage()): ?>
-        <div class="alert alert-<?= $this->getMessage('type'); ?>"><?= ucfirst($this->getMessage('message')); ?>.</div>
-    <?php endif; ?>
+        <?php if ($this->isMessage()): ?>
+            <div class="alert alert-<?= $this->getMessage('type'); ?>"><?= ucfirst($this->getMessage('message')); ?>.</div>
+        <?php endif; ?>
 
-    <label for="inputEmail" class="sr-only">Email</label>
-    <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Email"
-           value="<?= $this->getNick(); ?>" required autofocus>
+        <label for="inputEmail" class="sr-only">Email</label>
+        <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Email"
+            value="<?= $this->getNick(); ?>" required autofocus>
 
-    <label for="inputPassword" class="sr-only">Hasło</label>
-    <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Hasło" required>
+        <label for="inputPassword" class="sr-only">Hasło</label>
+        <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Hasło" required>
 
-    <button name="send" class="btn btn-lg btn-primary btn-block" type="submit">Zaloguj</button>
-    <p class="mt-5 mb-3 text-muted">&copy; by Rudolf <?= VER_NAME; ?></p>
-</form>
+        <button name="send" class="btn btn-lg btn-primary btn-block" type="submit">Zaloguj</button>
+        <p class="mt-5 mb-3 text-muted">&copy; by Rudolf <?= VER_NAME; ?></p>
+    </form>
+</div>
 </body>
 
 </html>
