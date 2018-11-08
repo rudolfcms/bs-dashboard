1d54454ec9cc9d091a96bd99fe64b841e8a7f3dc
Mikołaj Pich <m.pich@outlook.com>
2018-11-08 17:12:05 +0100

    Add version tag to styles link

--- theme.php
+++ theme.php
@@ -4,16 +4,16 @@ use Rudolf\Component\Html\Theme;
 
 class bs_dashboard extends Theme
 {
-    const VERSION = '2.0.0';
+    const VERSION = '2.0.1';
 
     public function init()
     {
         $this->addHeadBefore('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">');
-        $this->addStylesheet($this->path.'/css/bootstrap.min.css');
-        $this->addStylesheet('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css');
+        $this->addStylesheet($this->path.'/css/bootstrap.min.css', self::VERSION);
+        $this->addStylesheet('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', self::VERSION);
 
-        $this->addStylesheet($this->path.'/css/dashboard.css');
-        $this->addStylesheet($this->path.'/css/custom.css');
+        $this->addStylesheet($this->path.'/css/dashboard.css', self::VERSION);
+        $this->addStylesheet($this->path.'/css/custom.css', self::VERSION);
 
         $this->addFootBefore('<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script>window.jQuery || document.write("<script src=\''.$this->path.'/js/jquery.min.js\'><script>")</script>');
